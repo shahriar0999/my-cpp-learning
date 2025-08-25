e069b5731ac91f9846ddf26d7c4f03fc2f9ccb95
shahriar0999 <shahriar0999@proton.me>
2025-08-25 19:40:49 +0600

    comments in c++

---
+++ Day1/comments.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    cout<<"I'm learning C++"<<endl; // single line comment
+    /* multiline comment 
+    multiline comment */
+    
+    return 0;
+}
