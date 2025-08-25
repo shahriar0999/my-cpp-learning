b9e2425a8734ebffb0f17e523d99ea758c27f961
shahriar0999 <shahriar0999@proton.me>
2025-08-25 19:14:30 +0600

    display the output of program

--- Day1/display_output.cpp
+++ Day1/display_output.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout<<"I'm learning C++"<<endl;
+    cout<<"I'm learning C++"<<endl; // display string
+    cout<< 30 + 20 << endl;   // display number
+    cout<< "Sum of 30 and 20 \n" ; // we can use \n or endl to end the line of code
+    cout<< 3 * 12 << endl;
     return 0;
 }
