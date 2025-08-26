d963af6d40d8c9b065da891246bed67f81764431
shahriar0999 <shahriar0999@proton.me>
2025-08-26 23:55:53 +0600

    declare variables

---
+++ Day2/declare_variables.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 20; // Integer variable
+    double y = 3.33;
+    char z = 'Z'; // Double variable
+    string name = "Shahriar"; //string variable
+    bool isCodingfun = true; // boolean variable
+
+    return 0;
+}
