91eb4b0296f063af3b1aff973d672d8547a4f37b
shahriar0999 <shahriar0999@proton.me>
2025-08-28 20:00:06 +0600

    User input

---
+++ Day4/user_input.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n; // user input
+    cout << "Your number is : " << n << endl;
+    return 0;
+}
