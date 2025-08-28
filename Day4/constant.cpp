75c9e3cdadaa8933e150dbf6048b442f66ff2f08
shahriar0999 <shahriar0999@proton.me>
2025-08-28 19:56:12 +0600

    constant variable declaration

---
+++ Day4/constant.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // When you declare a constant variable, it must be assigned with a value
+    const string name = "Shahriar"; // constant variable
+    cout << "My name is: " << name << endl;
+}
