4e386dc7656adf59d0089101b9d6e8ebf78cb06a
shahriar0999 <shahriar0999@proton.me>
2025-08-28 19:50:48 +0600

    multiple variable declaration

---
+++ Day4/multiple_variable.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 4, y = 10, z = 6; // Multiple variable declaration in a single line
+    cout << "Sum of three numbers: " << x + y + z << endl;
+
+    // one value to multiple variables
+    int a, b, c;
+    a = b = c = 50;
+    cout << x + y + z << endl;
+    return 0;
+}
