9cfb898a83b68caca10fa813b94d23e222416c32
shahriar0999 <shahriar0999@proton.me>
2025-08-28 20:04:38 +0600

    simple calculator using user input

---
+++ Day4/calculator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int sum;
+    cout << "Type a number: ";
+    cin >> a; // usr input a
+    cout << "Type another number: ";
+    cin >> b; // user input b
+    sum = a + b; // sum of a and b
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
