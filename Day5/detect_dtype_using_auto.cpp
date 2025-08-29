21dac34f8611b9932ebdbb225218752f98a4f0ef
shahriar0999 <shahriar0999@proton.me>
2025-08-29 20:08:23 +0600

    detect data type of variables using 'auto' keyword

---
+++ Day5/detect_dtype_using_auto.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+// Creating auto variables
+auto myNum = 5; // int
+auto myFloatNum = 5.99; // float
+auto myDoubleNum = 9.98; // double
+auto myLetter = 'D'; // char
+auto myBoolean = true; // bool
+auto myString = string("Hello"); // std::string
+
+return 0;
+}
