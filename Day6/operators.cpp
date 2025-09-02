7a1f6361647912154725035d8e28a372a539ad13
shahriar0999 <shahriar0999@proton.me>
2025-09-02 09:50:42 +0600

    operators in c++

--- Day6/operators.cpp
+++ Day6/operators.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 
 int main(){
-    cout << "Hello, World!!!" << endl;
+    cout << "Operators in C++" << endl;
+    // take input
+    int n; cin >> n;
+    int sum = n + 30;
+    int sub = n - 1;
+    int mul = n * 4;
+    int div = n / 2;
+    int mod = n % 3;
+
+    cout << n << " " << sum << " " << sub << " " << mul << " " << div << " " << mod << endl;
     return 0;
 }
