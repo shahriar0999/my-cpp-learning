d0c71c9bcf237924c2c32232968dc05192f84cec
shahriar0999 <shahriar0999@proton.me>
2025-09-02 10:05:17 +0600

    Assignment Operators

---
+++ Day7/assignment_operators.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    // assignment operators
+    int age = 20 ;
+    cout << age << endl;
+    age += 3;
+    age -= 3;
+    age *= 3;
+    age /= 3;
+    cout << "Final Age after assignment operator " << age << endl;
+    return 0;
+}
