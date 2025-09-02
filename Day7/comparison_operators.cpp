b4bd339be2c26317f37e6570b06fc593f2223cdb
shahriar0999 <shahriar0999@proton.me>
2025-09-02 23:59:36 +0600

    Comparison operator

---
+++ Day7/comparison_operators.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int a, b; cin >> a >> b;
+    cout << (a == b) << endl;   // equal equal
+    cout << (a != b) << endl; // not equal
+    cout << (a >= b) << endl; // greater than eqal
+    cout << (a <= b) << endl; // less than equal
+}
