2a594c29a972f3e81890f8e8a95c92e2ea4208d4
shahriar0999 <shahriar0999@proton.me>
2025-09-04 13:25:55 +0600

    Word Capitalization

---
+++ Day9/capitalization_word.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/281/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name = "shahriar";
+    name[0] = toupper(name[0]);
+    cout << name << endl;
+    return 0;
+}
