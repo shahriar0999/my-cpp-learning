14311a6575d7053366745d36df75ebecf520ed14
shahriar0999 <shahriar0999@proton.me>
2025-09-04 09:19:04 +0600

    Length/Size of string

---
+++ Day9/string_length.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // calculate length using length() func
+    cout << "The length of txt string is : " << txt.length() << endl;
+
+    // calculate length using size() func
+    cout << "The size of txt string is : " << txt.size() << endl;
+    return 0;
+}
