930632953fcd86d2623ea70697de2d087fff364d
shahriar0999 <shahriar0999@proton.me>
2025-09-05 14:57:15 +0600

    math operation in c++

---
+++ Day10/math.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <cmath>
+
+
+int main(){
+    cout << max(10, 3) << endl;
+    cout << min(10, 3) << endl;
+    cout << sqrt(10) << endl;
+    return 0;
+}
