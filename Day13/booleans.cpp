5ff272da6507cd3a2c2461693f787eeb0eb2b758
shahriar0999 <shahriar0999@proton.me>
2025-09-12 22:31:28 +0600

    updated the file

--- Day13/booleans.cpp
+++ Day13/booleans.cpp
@@ -8,4 +8,11 @@ int main(){
 
     cout << "Output of a variable" << a << endl;
     cout << "Output of b variable" << b << endl;
+
+    int x = 10;
+    int y = 9;
+    cout << (x > y); // returns 1 (true), because 10 is higher than 9
+
+    return 0;
+
 }
