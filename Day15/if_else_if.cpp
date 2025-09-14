ccf79a01412bfd482a477f5764622d1b3923d4ba
shahriar0999 <shahriar0999@proton.me>
2025-09-14 23:50:30 +0600

    else_if -conditional statement

---
+++ Day15/if_else_if.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 30; 
+    int y = 20;
+
+    if (x > y){
+        cout << "x is greater than y" << endl;  // if condtion is true
+
+    }else if (x == y){
+        cout << "x equal to y" << endl; // if condition is false
+    }else{
+        cout << "x is less than y" << endl; // if and else if conditions are false
+    }
+
+    return 0;
+}
