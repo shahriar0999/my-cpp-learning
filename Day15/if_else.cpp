7189e9b240945dceb876249153fead6edbbc0703
shahriar0999 <shahriar0999@proton.me>
2025-09-14 23:41:45 +0600

    if -conditional statement

---
+++ Day15/if_else.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 30; 
+    int y = 20;
+
+    if (x > y){
+        cout << "x is greater than y" << endl;
+
+    }
+
+    return 0;
+}
