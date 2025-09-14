cf43dcdacb0e79bbea0fefca0cb09209b1985f33
shahriar0999 <shahriar0999@proton.me>
2025-09-14 23:56:36 +0600

    short hand if_else

---
+++ Day15/short_hand_if_else.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int time = 20;
+    string result = (time < 18) ? "Good day." : "Good evening.";
+    cout << result;
+    return 0;
+}
+
