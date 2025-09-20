f07e0d8839813e518edf197aa4fc81a0879ace4c
shahriar0999 <shahriar0999@proton.me>
2025-09-20 22:50:41 +0600

    do while loop in c++

---
+++ Day17/do_while_loop.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i = 1;
+    do {
+        cout << i << endl;
+        i++;  //incrementing the value of i by 1
+    }
+    while (i >= 10);
+    return 0;
+}
