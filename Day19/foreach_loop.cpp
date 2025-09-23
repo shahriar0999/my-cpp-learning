3b5511fcc4fe2e0fc0fc677a173763c52b2b8aab
shahriar0999 <shahriar0999@proton.me>
2025-09-24 00:04:25 +0600

    updated version

--- Day19/foreach_loop.cpp
+++ Day19/foreach_loop.cpp
@@ -1,4 +1,22 @@
-int myNumbers[5] = {10, 20, 30, 40, 50};
-for (int num : myNumbers) {
-  cout << num << "\n";
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for (int num : myNumbers) {
+    cout << num << "\n";
+    }
+
+    string word = "Hello!, World";
+    for (char c : word) {
+        cout << c << "\n";
+    }
+
+    return 0;
+
 }
+
+
+
+
