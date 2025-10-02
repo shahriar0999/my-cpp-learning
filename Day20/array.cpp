976f85684116596785d39719b040b8c4c6214c45
shahriar0999 <shahriar0999@proton.me>
2025-10-02 21:02:42 +0600

    array in c++

---
+++ Day20/array.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string student[4] = {"Shahriar", "Kabir", "Moninur", "Rahman"};
+    // print the second student
+    cout << student[1] << endl;
+
+    return 0;
+}
