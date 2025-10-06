11352e41547fa1f4f6807b4e89d028f5f6eb8781
shahriar0999 <shahriar0999@proton.me>
2025-10-06 21:08:27 +0600

    start day21 with Array

---
+++ Day21/array.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[4] = {1, 2, 3,4};
+
+    for (int i = 0; i < 4; i++) {
+        cout << arr[i] << "";
+        
+    }
+}
