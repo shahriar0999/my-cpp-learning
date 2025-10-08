fb8a82566db3a2abdbc84178d15821d4262dd58b
shahriar0999 <shahriar0999@proton.me>
2025-10-08 22:59:05 +0600

    one stractures with multiple variable

--- Day22/Structures.cpp
+++ Day22/Structures.cpp
@@ -12,3 +12,16 @@ int main() {
     cout << "x: " << MyStruct.x << ", y: " << MyStruct.y << endl;
     return 0;
 }
+
+// one structure with multiple variables
+struct Point {
+    int x;
+    int y;
+} p1, p2;
+
+// one structure with initialization
+struct Rectangle {
+    int length = 10;
+    int breadth = 5;
+} rect1, rect2;
+
