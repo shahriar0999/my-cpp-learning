9c30cb9ea1ee8fd6a5b2b8f186dd5ceb3a2e3ec5
shahriar0999 <shahriar0999@proton.me>
2025-10-09 19:48:10 +0600

    Parameters and Argument for function

--- Day23/function.cpp
+++ Day23/function.cpp
@@ -6,8 +6,18 @@ void add(int a, int b){
     cout << "The sum is: " << a + b << endl;
 }
 
+
+// parameter and argument
+void greet(string name){  // name is parameter
+    cout << "Hello, " << name << endl;
+}
+
+
 int main(){
     int a = 10, b = 20;
     add(a, b);
+
+    // call the greet function
+    greet("Alice"); // Here "Alice" is argument
     return 0;
 }
