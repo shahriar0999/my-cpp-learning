8efe64911e130a75870153642c5f78c9a7ac1d2d
shahriar0999 <shahriar0999@proton.me>
2025-10-09 19:43:22 +0600

    function in c++)

---
+++ Day23/function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create a function to add two numbers
+void add(int a, int b){
+    cout << "The sum is: " << a + b << endl;
+}
+
+int main(){
+    int a = 10, b = 20;
+    add(a, b);
+    return 0;
+}
