679a3e0edecc51e76d30c250b61bc1cb6c8321e7
shahriar0999 <shahriar0999@proton.me>
2026-02-16 23:50:41 +0600

    stair peak neither

---
+++ LeetCode/stair_peak_neither.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    for (int i=0; i < t; i++){
+        int a, b, c; cin >> a >> b >> c;
+
+        //A stair satisfies the condition a<b<c. A peak satisfies the condition a<b>c
+
+        if ((a<b && b<c)){
+            cout << "STAIR" << endl;
+        }
+        else if ((a < b && b > c)){
+            cout << "PEAK" << endl;
+        }
+        else{
+            cout << "NONE" << endl;
+        }
+
+   }
+    
+}
