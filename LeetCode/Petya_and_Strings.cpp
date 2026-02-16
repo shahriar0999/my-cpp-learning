78a787184cd00013eb91b8793b7fa2e6d9806ae4
shahriar0999 <shahriar0999@proton.me>
2026-02-16 23:55:22 +0600

    Petya and Strings

---
+++ LeetCode/Petya_and_Strings.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++){
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+    }
+
+    if (a < b) {
+        cout << -1 << endl;
+    }
+    else if (a > b) {
+        cout << 1 << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
