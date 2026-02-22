86378354a7e2e0b6d2279d562e276269745edd99
shahriar0999 <shahriar0999@proton.me>
2026-02-22 09:29:29 +0600

    first digit

---
+++ CodeForce/P_First_digit.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s; cin >> s;
+    char a = s[0] - '0';
+    if (a%2==0) cout << "EVEN" << endl;
+    else cout << "ODD" << endl;
+
+}
