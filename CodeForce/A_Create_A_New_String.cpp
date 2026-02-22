9e80d834ee3ac6f4e1cc60bcb48daf81fb497d08
shahriar0999 <shahriar0999@proton.me>
2026-02-22 08:48:57 +0600

    create a new string

---
+++ CodeForce/A_Create_A_New_String.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a, b; cin >> a >> b; 
+    cout << a.size() << " " << b.size() << endl;
+    cout << a << " " << b << endl;
+}
