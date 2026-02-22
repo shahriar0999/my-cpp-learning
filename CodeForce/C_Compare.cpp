53426a65de73620f2cb6c815bcc13046406f0a81
shahriar0999 <shahriar0999@proton.me>
2026-02-22 10:12:18 +0600

    compare two string

---
+++ CodeForce/C_Compare.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s; cin >> s;
+    string t; cin >> t;
+
+    if (s < t) cout << s << endl;
+    else cout << t << endl;
+
+}
